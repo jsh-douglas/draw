a7db3d230332147bc2c9b57b063f573b6764b303
Josh Douglas <josh.douglas0108@gmail.com>
2020-05-04 13:28:22 +0100

    Brush and Rubber now use independant sizes and colors 	modified:   index.html 	modified:   script.js

--- script.js
+++ script.js
@@ -1,51 +1,71 @@
-let brush = {
-    enabled: true,
-    active: false,
-    width: 10,
-    color: "rgb(0, 0, 0)"
+let tools = {
+    brush: {
+        enabled: true,
+        active: false,
+        width: 10,
+        color: 'rgb(0, 0, 0)'
+    },
+    rubber: {
+        enabled: false,
+        active: false,
+        width: 10,
+        color: 'rgb(255, 255, 255)'
+    },
+    fill: {
+        enabled: false,
+    },
+    enabled: 'brush'
 }
-
-// let rubber = {
-//     enabled: false,
-//     active: false,
-//     width: 10,
-//     color: "rgb(255, 255, 255)"
-// }
 
 let root = document.documentElement;
 let canvas = document.getElementById('main-canvas');
 
 function init() {
     if (canvas.getContext) {
-
         canvas.width = 800;
         canvas.height = 600;
 
         ctx = canvas.getContext("2d");
+        ctx.lineCap = "round";
 
         canvas.addEventListener('mousedown', (event) => {
-            ctx.lineWidth = brush.width;
-            ctx.lineCap = "round";
-            brush.active = true;
-            ctx.beginPath();
-            ctx.strokeStyle = brush.color;
-            ctx.lineTo(event.offsetX, event.offsetY);
-            ctx.stroke()
+            for (const tool in tools) {
+                if (tools[tool].enabled) {
+                    tools.enabled = tool;
+                    switch (tool) {
+                        case 'brush':
+                        case 'rubber':
+                            tools[tool].active = true;
+                            ctx.lineWidth = tools[tool].width;
+                            ctx.strokeStyle = tools[tool].color;
+
+                            ctx.beginPath();
+                            ctx.lineTo(event.offsetX, event.offsetY);
+                            ctx.stroke()
+                            break
+                        case 'fill':
+                            break
+                    }
+                }
+            }         
         })
 
         document.addEventListener('mousemove', (event) => { 
             if (event.target == canvas) {
-                if (brush.active) {
-                    ctx.lineTo(event.offsetX, event.offsetY);
-                    ctx.stroke();
+                if (tools.enabled == 'brush' || tools.enabled == 'rubber') {
+                    if (tools[tools.enabled].active) {
+                        ctx.lineTo(event.offsetX, event.offsetY);
+                        ctx.stroke();
+                        ctx.beginPath();
+                        ctx.moveTo(event.offsetX, event.offsetY);
+                    }
                 }
-            } else {
-                // brush.active = false;
             }
         })
 
         document.addEventListener('mouseup', () => {
-            brush.active = false;
+            tools[tools.enabled].active = false;
+            ctx.closePath();
         })
 
         
@@ -57,23 +77,48 @@ function init() {
 window.onload = init();
 
 
-function updateBrushColor(obj) {
+function setBrushColor(obj) {
     if (obj.checked) {
         color = getComputedStyle(root).getPropertyValue(obj.dataset.color);
+        tools.brush.color = color;
         root.style.setProperty('--active-color', color);
-        brush.color = color;
+        document.getElementById('tool--brush').checked = true;
+        setTool('brush');
     }
 }
 
-function updateBrushSize(width) {
-    brush.width = width;
+function setToolSize(obj) {
+    if (obj.checked) {
+        tools[tools.enabled].width = parseInt(obj.dataset.size);
+    }
 }
 
 function clearCanvas() {
     canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
 }
 
-function rubber() {
-    // brush.color = 'rgb(255, 255, 255)';
-    return
+function setTool(tool) {
+    tools.enabled = tool;
+    switch (tool) {
+        case 'brush':
+            tools.brush.enabled = true;
+            tools.rubber.enabled = false;
+            tools.fill.enabled = false;
+            document.querySelectorAll(`[data-size='${tools[tool].width}']`)[0].checked = true;
+            root.style.setProperty('--active-color', tools[tool].color);
+            break
+        case 'rubber':
+            tools.brush.enabled = false;
+            tools.rubber.enabled = true;
+            tools.fill.enabled = false;
+            document.querySelectorAll(`[data-size='${tools[tool].width}']`)[0].checked = true;
+            root.style.setProperty('--active-color', tools[tool].color);
+            break
+        case 'fill':
+            tools.brush.enabled = false;
+            tools.rubber.enabled = false;
+            tools.fill.enabled = true;
+    }
 }
+
+
